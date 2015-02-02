39d5bf3e947a57e66611d9de3f1d308dbf520dff
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2015-02-02 20:45:41 +0100

    Initial Commit

---
+++ Labb3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb3
+//
+//  Created by it-högskolan on 2015-02-02.
+//  Copyright (c) 2015 it-högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Labb3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Labb3
+//
+//  Created by it-högskolan on 2015-02-02.
+//  Copyright (c) 2015 it-högskolan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
